deaa115cfe590b4fdadce3b16babd24f73a92db9
Chanon <pcg.mega@gmail.com>
2020-09-12 23:06:45 +0700

    shorten

--- Circle/Source.cpp
+++ Circle/Source.cpp
@@ -7,19 +7,11 @@ int main()
     int radius;
     scanf_s("%d", &radius);
     int row, j;
-    for (row = 0; row <= 2 * radius; row++)
-    {
-        for (j = 0; j <= 2 * radius; j++)
-        {
+    for (row = 0; row <= 2 * radius; row++) {
+        for (j = 0; j <= 2 * radius; j++) {
             double distance = sqrt((row - radius) * (row - radius) + (j - radius) * (j - radius));
-            if (distance > radius - 0.5 && distance < radius + 0.5)
-            {
-                printf("[]");
-            }
-            else
-            {
-                printf("  ");
-            }
+            if (distance > radius - 0.5 && distance < radius + 0.5) printf("[]");
+            else printf("  ");
         }
         printf("\n");
     }
